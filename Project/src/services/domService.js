a858b3433a7749528ad8c862b3add9afb03e5331
Victor Yampolsky <tsanhan@gmail.com>
2022-12-19 20:02:12 +0200

    page model

--- Project/src/services/domService.js
+++ Project/src/services/domService.js
@@ -6,6 +6,6 @@ export const ABOUT_PAGE_LINK = document.querySelector('#about-page-link');
 //#endregion
 
 //#region pages
-
-
+export const HOME_PAGE = document.querySelector('#home-page');
+export const ABOUT_PAGE = document.querySelector('#about-page');
 //#endregion
