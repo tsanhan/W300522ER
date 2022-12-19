3ae6c88939b1a175c9e5fa93ae403f5fa157a4e6
Victor Yampolsky <tsanhan@gmail.com>
2022-12-19 20:28:20 +0200

    class assignment

--- Project/src/app.js
+++ Project/src/app.js
@@ -8,3 +8,14 @@ HOME_PAGE_LINK.addEventListener('click', () => onChangePage(PAGES.HOME));
 ABOUT_PAGE_LINK.addEventListener('click', () => onChangePage(PAGES.ABOUT));
 
 //#endregion
+
+
+// class assignment:
+// 1. I want to be able to bounce between pages: 'home', 'about', 'add pic' and login.
+// 2. create another page for 404 (in case the user got into a page that did not match any of the pages)
+// 3. create a page for 'add pic' and 'login'
+// 4. create links for 'add pic' and 'login'
+// 5. add a nice title and subtitle to the pages
+// 6. add the the page model keys and values for there pages
+// 7. add the pages to the domService
+// 8. add event listeners for the links in the nav for these pages 
