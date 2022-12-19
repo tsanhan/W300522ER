008bee3927d3d361c13566e89b7a8465918f25ea
Victor Yampolsky <tsanhan@gmail.com>
2022-12-19 19:50:43 +0200

    pages 1

---
+++ Project/src/services/domService.js
@@ -0,0 +1,11 @@
+//#region nav links
+export const HOME_PAGE_LINK = document.querySelector('#home-link');
+export const ABOUT_PAGE_LINK = document.querySelector('#about-page-link');
+
+
+//#endregion
+
+//#region pages
+
+
+//#endregion
