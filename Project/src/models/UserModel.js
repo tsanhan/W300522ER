748a180816177c45d757bee28982f038e7653286
Victor Yampolsky <tsanhan@gmail.com>
2022-12-29 20:54:50 +0200

    validation

--- Project/src/models/UserModel.js
+++ Project/src/models/UserModel.js
@@ -18,10 +18,10 @@ class User {
     #isAdmin = false;
     #isBusiness = false;
     //#endregion
-    
+
     constructor(user, users = []) {
         const {
-            name,
+            name: { first, last },
             address: {
                 state,
                 country,
@@ -38,10 +38,10 @@ class User {
         } = user;
 
         this.generateId(users);
-        this.#name = name;
-        this.#phone = phone;
-        this.#email = email;
-        this.#password = password;
+        this.#name = this.setName(first, last);
+        this.#phone = this.checkPhone(phone);
+        this.#email = this.checkMail(email, users);
+        this.#password = this.checkPassword(password);
         this.#isAdmin = isAdmin;
         this.#isBusiness = isBusiness;
         this.address = { state, country, city, street, houseNumber, zip };
@@ -69,7 +69,66 @@ class User {
         this.#name = `${firstName} ${lastName}`;
     }
 
-    
+    changeBusinessStatus(user) {
+        if (!user.isAdmin) return null;
+        this.#isBusiness = !this.#isBusiness;
+    }
+
+    checkPassword(password) {
+        const rg = /(?=.*\d{1})(?=.*\d{1})(?=.*\d{1})(?=.*\d{1})(?=.*[A-Z]{1})(?=.*[a-z]{1})(?=.*[!@#$%^&*-]{1}).{7,20}/g;
+        if (password.match(rg) !== null) return password;
+        throw new Error('the password must contain at least 1 uppercase letter in English, 1 lowercase letter in English. 4 numbers and one of the following characters: !@#$%^&*-');
+    }
+
+    checkPhone(phone) {
+        const rg = /^0[0-9]{1,2}(\-|\s?)[0-9]{3}(\-|\s?)[0-9]{4}/g;
+        if (phone.match(rg) !== null) return phone;
+        throw new Error('the phone should be valid');
+    }
+
+    checkMail(email, users = []) { 
+        const rg = /^.+@.+\..{2,}$/g;
+        if (email.match(rg) === null) throw new Error('the email should be valid');
+        
+        const user = users.find(usr => usr.email === email);
+        if (!user) return email;
+        throw new Error('this email is already in use');
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    get password() {
+        return this.#password;
+    }
+
+    get createdAt() {
+        return this.#createdAt;
+    }
+
+    get isAdmin() {
+        return this.#isAdmin;
+    }
+
+    get isBusiness() {
+        return this.#isBusiness;
+    }
+    get phone() {
+        return this.#phone;
+    }
+
+    set isBusiness(value) {
+        this.#isBusiness = value;
+    }
+
+    // setter for phone
+    // setter for name
+
 }
 
 export default User;
