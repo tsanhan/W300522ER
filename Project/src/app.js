ec7e4b58440863bc6bab83423f6c07ea23f03848
VICTOR YAMPOLSKT <VICTOR.YAMPOLSKY@RETAILIN.CO>
2023-01-05 19:31:08 +0200

    export some functions to picService

--- Project/src/app.js
+++ Project/src/app.js
@@ -28,7 +28,7 @@ import User from "./models/UserModel.js";
 
 import { onChangePage } from "./routes/router.js";
 import { renderSlider as render } from "./services/renderSlider.js";
-import { setCounter } from "./services/picService.js";
+import { handleCancelCreatePic, handleCreatePic, onCreateNewPic, setCounter } from "./services/picService.js";
 
 import initialData from "./initialData/initialData.js";
 
@@ -72,64 +72,6 @@ SLIDER_NEXT_BTN.addEventListener('click', () => onChangeSliderPic('next'));
 
 //#region Create Picture
 
-export const handleCreatePic = () => {
-    // הגענו לדף
-    onChangePage(PAGES.CREATE_PIC);
-
-    // להרשם לאירועי הזנת המידע בשדות
-    createPicFromFieldsListeners();
-
-    CANCEL_BTN.addEventListener('click', () => {
-        const conf = confirm('Are you sure you want to cancel?')
-        if (conf) handleCancelCreatePic()
-    });
-
-    SUBMIT_CREATE_PIC_BTN.addEventListener('click', () => handleSubmitNewPic());
-}
-
-export const createPicFromFieldsListeners = () => {
-    const { onChangeInputField } = useForm();
-    const schema = ['url', 'price'];
-    URL_CREATE_PIC_FIELD.addEventListener('input', e => {
-        const validation = {
-            regex: /^http[s]?\:\/\/.{1,}.(jpg|png|jpeg)$/g,
-            min: 10,
-            lowerCase: true
-        };
-
-        const element = {
-            input: e.target,
-            errorSpan: URL_CREATE_PIC_ERROR,
-            validation
-        }
-
-        onChangeInputField(schema, element, SUBMIT_CREATE_PIC_BTN);
-    });
-
-
-    PRICE_CREATE_PIC_FIELD.addEventListener('input', e => {
-        const validation = {
-            regex: /^\d+$/,
-            numMin: 100
-        };
-
-        const element = {
-            input: e.target,
-            errorSpan: PRICE_CREATE_PIC_ERROR,
-            validation
-        };
-
-        onChangeInputField(schema, element, SUBMIT_CREATE_PIC_BTN);
-    });
-}
-
-export const handleCancelCreatePic = () => {
-    const { onClearFormFields } = useForm();
-    const fields = [URL_CREATE_PIC_FIELD, ALT_CREATE_PIC_FIELD, CREDIT_CREATE_PIC_FIELD, PRICE_CREATE_PIC_FIELD];
-    const errorSpans = [URL_CREATE_PIC_ERROR, ALT_CREATE_PIC_ERROR, CREDIT_CREATE_PIC_ERROR, PRICE_CREATE_PIC_ERROR];
-    onClearFormFields(SUBMIT_CREATE_PIC_BTN, fields, errorSpans);
-    onChangePage(PAGES.HOME);
-}
 
 export const handleSubmitNewPic = () => {
     pictures = onCreateNewPic(pictures);
@@ -137,20 +79,5 @@ export const handleSubmitNewPic = () => {
     handleCancelCreatePic();
 }
 
-export const onCreateNewPic = pictures => {
-    try {
-        let newArray = [...pictures];
-        const pic = new Picture({
-            url: URL_CREATE_PIC_FIELD.value,
-            alt: ALT_CREATE_PIC_FIELD.value,
-            credit: CREDIT_CREATE_PIC_FIELD.value,
-            price: PRICE_CREATE_PIC_FIELD.value
-        },newArray);
-        newArray.push(pic);
 
-        return newArray;
-    } catch (error) {
-        console.log(error.message);
-    }
-};
 //#endregion
--- Project/src/services/picService.js
+++ Project/src/services/picService.js
@@ -1,3 +1,10 @@
+import { handleSubmitNewPic } from "../app.js";
+import PAGES from "../models/pageModel.js";
+import Picture from "../models/PictureModel.js";
+import { onChangePage } from "../routes/router.js";
+import { ALT_CREATE_PIC_ERROR, ALT_CREATE_PIC_FIELD, CANCEL_BTN, CREDIT_CREATE_PIC_ERROR, CREDIT_CREATE_PIC_FIELD, PRICE_CREATE_PIC_ERROR, PRICE_CREATE_PIC_FIELD, SUBMIT_CREATE_PIC_BTN, URL_CREATE_PIC_ERROR, URL_CREATE_PIC_FIELD } from "./domService.js";
+import useForm from "./formService.js";
+
 export const setCounter = (array, counter, controller = "") => {
     let newCounter;
     if (controller === "next") {
@@ -12,3 +19,78 @@ export const setCounter = (array, counter, controller = "") => {
     
     return 0;
 }
+
+
+export const handleCreatePic = () => {
+    // הגענו לדף
+    onChangePage(PAGES.CREATE_PIC);
+
+    // להרשם לאירועי הזנת המידע בשדות
+    createPicFromFieldsListeners();
+
+    CANCEL_BTN.addEventListener('click', () => {
+        const conf = confirm('Are you sure you want to cancel?')
+        if (conf) handleCancelCreatePic()
+    });
+
+    SUBMIT_CREATE_PIC_BTN.addEventListener('click', () => handleSubmitNewPic());
+}
+
+export const createPicFromFieldsListeners = () => {
+    const { onChangeInputField } = useForm();
+    const schema = ['url', 'price'];
+    URL_CREATE_PIC_FIELD.addEventListener('input', e => {
+        const validation = {
+            regex: /^http[s]?\:\/\/.{1,}.(jpg|png|jpeg)$/g,
+            min: 10,
+            lowerCase: true
+        };
+
+        const element = {
+            input: e.target,
+            errorSpan: URL_CREATE_PIC_ERROR,
+            validation
+        }
+
+        onChangeInputField(schema, element, SUBMIT_CREATE_PIC_BTN);
+    });
+
+
+    PRICE_CREATE_PIC_FIELD.addEventListener('input', e => {
+        const validation = {
+            regex: /^\d+$/,
+            numMin: 100
+        };
+
+        const element = {
+            input: e.target,
+            errorSpan: PRICE_CREATE_PIC_ERROR,
+            validation
+        };
+
+        onChangeInputField(schema, element, SUBMIT_CREATE_PIC_BTN);
+    });
+}
+export const handleCancelCreatePic = () => {
+    const { onClearFormFields } = useForm();
+    const fields = [URL_CREATE_PIC_FIELD, ALT_CREATE_PIC_FIELD, CREDIT_CREATE_PIC_FIELD, PRICE_CREATE_PIC_FIELD];
+    const errorSpans = [URL_CREATE_PIC_ERROR, ALT_CREATE_PIC_ERROR, CREDIT_CREATE_PIC_ERROR, PRICE_CREATE_PIC_ERROR];
+    onClearFormFields(SUBMIT_CREATE_PIC_BTN, fields, errorSpans);
+    onChangePage(PAGES.HOME);
+}
+export const onCreateNewPic = pictures => {
+    try {
+        let newArray = [...pictures];
+        const pic = new Picture({
+            url: URL_CREATE_PIC_FIELD.value,
+            alt: ALT_CREATE_PIC_FIELD.value,
+            credit: CREDIT_CREATE_PIC_FIELD.value,
+            price: PRICE_CREATE_PIC_FIELD.value
+        },newArray);
+        newArray.push(pic);
+
+        return newArray;
+    } catch (error) {
+        console.log(error.message);
+    }
+};
